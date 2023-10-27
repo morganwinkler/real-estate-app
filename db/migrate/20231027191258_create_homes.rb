e8c8295dbf9122b0736ff80233153c36deaf4c97
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-27 15:25:57 -0400

    Generate jbuilder views

--- db/migrate/20231027191258_create_homes.rb
+++ db/migrate/20231027191258_create_homes.rb
@@ -3,13 +3,13 @@ class CreateHomes < ActiveRecord::Migration[7.0]
     create_table :homes do |t|
       t.text :description
       t.integer :year_built
-      t.decimal :square_feet
+      t.decimal :square_feet, scale: 0, precision: 5
       t.integer :bedrooms
       t.integer :bathrooms
       t.integer :floors
       t.boolean :availability
       t.string :address
-      t.decimal :price
+      t.decimal :price, scale: 0, precision: 8
 
       t.timestamps
     end
